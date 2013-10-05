c853e886227239f605fe92709713b3d528ca5eb2
Kaique da Silva <kaique.developer@gmail.com>
2013-10-05 19:11:21 -0300

    CRUD completed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var finance = require('./finance');
 root.setup = function ( app ) {
     app.namespace('/api', function () {
         app.namespace('/finance', function () {
-            app.post('/earning', finance.earning);
+            app.namespace('/earning', function () {
+                app.post('/', finance.earning.create);    
+                app.put('/:id', finance.earning.update);
+                app.get('/:id', finance.earning.read);
+                app.del('/:id', finance.earning.del);
+            });
         }); 
     });    
 };
--- test/finance.test.js
+++ test/finance.test.js
@@ -2,6 +2,7 @@ var app = require('../app');
 var superagent = require('superagent');
 var should = require('should');
 var url = require('url');
+var ch = require('charlatan');
 
 app.listen( 3000 );
 
@@ -9,9 +10,10 @@ describe('Finance', function () {
     var target = 'http://localhost:3000';
     var agent = superagent.agent();
     var earning = {
-        title: 'salary',
-        value: 2000
+        title: ch.Name.title(),
+        value: ch.numerify('####') 
     };
+    var earningID = null;
 
     describe('earnings', function () {
         it('post', function ( done ) {
@@ -21,6 +23,8 @@ describe('Finance', function () {
                 res.body.id.should.have.lengthOf( 24 );
                 res.body.message.should.include('Earning was added!');
                 res.body.status.should.be.ok;
+
+                earningID = res.body.id;
                 done(); 
             }
 
@@ -29,5 +33,77 @@ describe('Finance', function () {
                 .send( earning )
                 .end( requestHandler );
         });    
+
+        it('put', function ( done ) {
+            earning = {
+                title: ch.Name.title(),
+                value: ch.numerify('####') 
+            };
+
+            function requestHandler ( e, res ) {
+                should.not.exist( e );
+                res.status.should.be.eql( 200 );
+                res.body.message.should.include('Earning was updated!');
+                res.body.status.should.be.ok;
+
+                done(); 
+            }
+
+            agent
+                .put( 
+                    url.resolve( 
+                        target, 
+                        ( '/api/finance/earning/' + 
+                          earningID )
+                    ) 
+                )
+                .send( earning )
+                .end( requestHandler );
+        });
+
+        it('get', function ( done ) {
+            function requestHandler ( e, res ) {
+                should.not.exist( e );
+                res.status.should.be.eql( 200 );
+                should.exist( res.body.result );
+                res.body.message.should.include('Earning was readed!');
+                res.body.status.should.be.ok;
+
+                done(); 
+            }
+
+            agent
+                .get( 
+                    url.resolve( 
+                        target, 
+                        ( '/api/finance/earning/' + 
+                          earningID )
+                    ) 
+                )
+                .send( earning )
+                .end( requestHandler );
+        });
+
+        it('delete', function ( done ) {
+            function requestHandler ( e, res ) {
+                should.not.exist( e );
+                res.status.should.be.eql( 200 );
+                res.body.message.should.include('Earning was removed!');
+                res.body.status.should.be.ok;
+
+                done(); 
+            }
+
+            agent
+                .del( 
+                    url.resolve( 
+                        target, 
+                        ( '/api/finance/earning/' + 
+                          earningID )
+                    ) 
+                )
+                .send( earning )
+                .end( requestHandler );
+        });
     });
 });
